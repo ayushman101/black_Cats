4c955adbf9160705dc10106d057100e5a10bc716
Ayushman Singh <amansinghayush@gmail.com>
2023-10-12 23:56:45 +0530

    server created

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const express= require('express');
+const app= express();
+
+app.use(express.json())
+
+const start= async ()=>{
+
+	try{
+		app.listen(3000, ()=> {
+			console.log("Server has started at port: 3000")
+		})
+	}
+
+	catch (error){
+		console.log(error);
+	}
+}
+
+start();
