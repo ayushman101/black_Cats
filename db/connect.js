a5f64cbb45a1039b2f92ed5d9b12cc89e7d359b3
Ayushman Singh <amansinghayush@gmail.com>
2023-10-14 13:09:16 +0530

    database mongoose connected

---
+++ db/connect.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose')
+
+const connectDB= (url)=> {
+
+	mongoose.connect(url, {
+
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+
+	}).then(()=>{
+		console.log("DB connected")
+	}).catch((error)=>{
+		console.log(error)
+	})
+}
+
+module.exports=connectDB;
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express= require('express');
 const app= express();
 require('dotenv').config()
+const connectDB=require('./db/connect')
 
 app.use(express.json())
+
 
 
 
 const start= async ()=>{
 
 	try{
+		await connectDB(process.env.MONGO_URL)
 		app.listen(process.env.PORT, ()=> {
 			console.log("Server has started at port: 3000")
 		})
