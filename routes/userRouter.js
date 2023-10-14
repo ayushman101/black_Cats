a640376574db04afe18c97797f82a9dae0f8ffea
Ayushman Singh <amansinghayush@gmail.com>
2023-10-14 15:53:07 +0530

    login route added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,10 @@ const express= require('express')
 const router= express.Router();
 
 const register=require('../controllers/register')
+const login= require('../controllers/login')
 
 router.route('/register').post(register)
+
+router.route('/login').post(login)
 
 module.exports=router;
